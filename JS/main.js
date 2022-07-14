a8a17425979bcfd688496c9ee9b0c820d341d370
reyandres015 <73958988+reyandres015@users.noreply.github.com>
2022-07-14 17:26:03 -0500

    Responsive Menu JS

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+document.getElementById ("btn_menu").addEventListener("click", mostrar_menu);
+document.getElementById("back_menu").addEventListener("click", ocultar_menu)
+
+nav = document.getElementById("nav_container");
+background_menu = document.getElementById("back_menu");
+
+function mostrar_menu(){
+    nav.style.right = "0px";
+    nav.style.display = "block"
+    background_menu.style.display = "block";
+
+}
+
+function ocultar_menu(){
+    nav.style.right = "-275px";
+    nav.style.display = "none"
+    background_menu.style.display = "none";
+
+}
+
