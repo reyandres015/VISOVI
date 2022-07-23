29f77502537dcf7611dca9b76dae409a4b55013a
reyandres015 <73958988+reyandres015@users.noreply.github.com>
2022-07-23 11:49:38 -0500

    Changes
    -x en nav
    -Cambio de Fondo
    -Incluidas Imagenes
    -Eliminar Nuestros Productos

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 document.getElementById ("btn_menu").addEventListener("click", mostrar_menu);
-document.getElementById("back_menu").addEventListener("click", ocultar_menu)
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+document.getElementById("btn_exit").addEventListener("click", ocultar_menu);
 
 nav = document.getElementById("nav_container");
 background_menu = document.getElementById("back_menu");
@@ -7,15 +8,12 @@ background_menu = document.getElementById("back_menu");
 function mostrar_menu(){
     nav.style.right = "0px";
     nav.style.display = "block";
-    nav.style.transition = "all 300ms";
     background_menu.style.display = "block";
-
 }
 
 function ocultar_menu(){
-    nav.style.right = "-275px";
     nav.style.display = "none";
     background_menu.style.display = "none";
-
 }
 
+console.log("Hello World, Como estais")
